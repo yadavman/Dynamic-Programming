699eed702cfe015baffdb1bf7207b9b0cb1aa8bd
Manish <Manish@LAPTOP-BDVGEQAL>
2021-04-02 17:31:36 +0530

    Unbounded Knapsack-Rod Cutting Problem

---
+++ RodCuttingProblem/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace RodCuttingProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Rod Cutting Problem!");
+
+            int[] length = { 1, 2, 3, 4, 5, 6, 7, 8 }; //Possible ways to cut the Rod
+
+            int[] price = { 3 , 5 ,  8,   9  ,10 , 17 , 17 , 20 }; // price for each length cut
+
+            int rodlength = 8; //needed size of rod
+
+            UnboundedKnapsack unboundedKnapsack = new UnboundedKnapsack();
+
+            Console.WriteLine("Max profit is {0}",unboundedKnapsack.MaximimizeProfitForRodCutting(length,price,rodlength,length.Length));
+
+            Console.Read();
+            //Maximize the Profit 
+        }
+    }
+}

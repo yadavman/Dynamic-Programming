5f322c7eb968291073542d9278d12d259375fd6b
Manish <Manish@LAPTOP-BDVGEQAL>
2021-04-02 15:08:40 +0530

    SubSet Sum Problem

---
+++ SubSet Sum Count/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SubSet_Sum_Count
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("SubSet Sum Count!");
+
+            int[] arr = { 2, 3, 5,7, 8, 10 };
+            //int sum = 10;
+
+            //PS: is ther any subset whose sum is equal to the given sum
+            Console.WriteLine("Given Array is ");
+            foreach (var item in arr)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+            SubSetSum subsetSum = new SubSetSum();
+
+            Console.WriteLine("Subset is {0} with sum {1}", subsetSum.IsSubsetPresent(arr, 167) ? "Present" : "Not Present", 167);
+
+            Console.WriteLine("Subset is {0} with sum {1}", subsetSum.IsSubsetPresent(arr, 11) ? "Present" : "Not Present", 11);
+
+            Console.WriteLine("Subset is {0} with sum {1}", subsetSum.IsSubsetPresent(arr, 5) ? "Present" : "Not Present", 5);
+
+            Console.WriteLine("Count of subset with given sum is {0}",subsetSum.CountSubSetForSum(arr,10));
+
+            Console.Read();
+        }
+    }
+}
---
+++ Subset Sum/SubsetSum.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Subset_Sum
+{
+    public class SubsetSum
+    {
+        
+        public SubsetSum()
+        {
+
+        }
+
+        public bool IsSubsetPresent(int[] arr, int sum)
+        {
+            bool[,] dp = new bool[arr.Length + 1, sum + 1];
+            // If sum is 0, then answer is true
+            for (int i = 0; i < arr.Length+1; i++)
+                dp[i, 0] = true;
+
+            // If sum is not 0 and set is empty,
+            // then answer is false
+            for (int i = 1; i <= sum; i++)
+                dp[0,i] = false;
+
+            //n rperestnts size of array
+            //k represent sum at each sub problems
+            for (int n = 1; n < dp.GetLength(0); n++)
+            {
+                for (int k = 1; k < dp.GetLength(1); k++)
+                {
+                    
+
+                    if (arr[n-1]>k)
+                    {
+                        //cannot include in the choice
+                        dp[n, k] = dp[n - 1, k];
+                    }
+                    else
+                    {
+                        //we have choice either to include or not to include
+                        //when include we need to reduce the sum with the value in the array else go to next element
+
+                        dp[n, k] = (dp[n - 1, k-arr[n-1]]) || (dp[n - 1, k]);
+
+                    }
+                }
+            }
+            return dp[arr.Length, sum];
+        }
+    }
+}

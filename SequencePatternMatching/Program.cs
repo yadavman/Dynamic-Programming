d75817aaed596336f74bace4b8beae7d89dc5096
Manish <Manish@LAPTOP-BDVGEQAL>
2021-04-03 18:39:11 +0530

    Seuqnece Pattern Matching

---
+++ SequencePatternMatching/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SequencePatternMatching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Seuqnece Pattern Matching!");
+            //Or we can simply do LCS of two strings and if LCS length is equal to first string Length then the string itself exists
+
+            string a = "AXM";
+            string b = "ADXCPY";
+            string str1 = "gksrek";
+            string str2 = "geeksforgeeks";
+            SequencePatternMatching spm = new SequencePatternMatching();
+
+            Console.WriteLine("The first sequence is {0}",spm.IsSequencePresent(a,b)?"Present":"Not Present");
+
+            Console.WriteLine("The first sequence is {0}", spm.IsSequencePresent(str1, str2) ? "Present" : "Not Present");
+            Console.Read();
+        }
+    }
+}

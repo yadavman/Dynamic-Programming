28fefcb8c49b9b2d8a2b578ee70a15495a0d02d4
Manish <Manish@LAPTOP-BDVGEQAL>
2021-04-03 17:27:57 +0530

    Longest Reating Subsequence

---
+++ Longest Repating Subsequence/LongestRepeatingSubsequence.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Longest_Repating_Subsequence
+{
+    public class LongestRepeatingSubsequence
+    {
+        public LongestRepeatingSubsequence()
+        {
+
+        }
+
+        public int LRSCount(string str)
+        {
+            int n = str.Length;
+
+            int[,] dp = new int[n+1,n+1];
+
+            for (int i = 0; i <=n; i++)
+            {
+                for (int j = 0; j <=n; j++)
+                {
+                    //Initailize
+                    if(i==0 || j == 0)
+                    {
+                        dp[i, j] = 0;
+                        continue;
+                    }
+
+                    //Apply LCS with restrcition that repeating char should not be at the same index
+
+                    if (str[i-1]==str[j-1] && i!=j)
+                    {
+                        dp[i, j] = 1 + dp[i - 1, j - 1];
+                    }
+                    else
+                    {
+                        dp[i, j] = Math.Max(dp[i - 1, j], dp[i, j - 1]);
+                    }
+                }
+            }
+            return dp[n, n];
+
+        }
+
+        //Repating coe for practice, others should appy common code 
+        public string LRS(string str)
+        {
+            int n = str.Length;
+
+            int[,] dp = new int[n + 1, n + 1];
+
+            for (int i = 0; i <= n; i++)
+            {
+                for (int j = 0; j <= n; j++)
+                {
+                    //Initailize
+                    if (i == 0 || j == 0)
+                    {
+                        dp[i, j] = 0;
+                        continue;
+                    }
+
+                    //Apply LCS with restrcition that repeating char should not be at the same index
+
+                    if (str[i - 1] == str[j - 1] && i != j)
+                    {
+                        dp[i, j] = 1 + dp[i - 1, j - 1];
+                    }
+                    else
+                    {
+                        dp[i, j] = Math.Max(dp[i - 1, j], dp[i, j - 1]);
+                    }
+                }
+            }
+
+            int index = dp[n,n]; //LRS string
+            char[] lrsArray = new char[index];
+            int p = n;
+            int q = n;
+
+            while(p>0 && q > 0)
+            {
+                if(str[p-1]==str[q-1] && p != q)
+                {
+                    lrsArray[index - 1] = str[p - 1];
+                    index--;
+                    p--;
+                    q--;
+                }
+                else
+                {
+                    if(dp[q,p-1]> dp[q-1, p])
+                    {
+                        p--;
+                    }
+                    else
+                    {
+                        q--;
+                    }
+                }
+            }
+
+
+            return new string(lrsArray);
+        }
+    }
+}
---
+++ Longest Repating Subsequence/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Longest_Repating_Subsequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Longest Repeating Subeequence!");
+
+            string str = "aabebcdd";
+            //o/p abd and count 3
+
+
+            LongestRepeatingSubsequence lps = new LongestRepeatingSubsequence();
+
+            Console.WriteLine("Longest Repeating Subsequence Count is {0}",lps.LRSCount(str));
+
+            Console.WriteLine("Longest Repeating Subsequence  is {0}", lps.LRS(str));
+            Console.Read();
+        }
+    }
+}
